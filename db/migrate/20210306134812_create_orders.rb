79cafde3a6a151b409a0083e3b19976f0546ef1b
Lourenço Silva Júnior <lourenco.silva72@gmail.com>
2021-03-06 16:01:09 -0300

    Alter table II Modify field data

--- db/migrate/20210306134812_create_orders.rb
+++ db/migrate/20210306134812_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
-      t.data :data
+      t.timestamps:data
       t.Integer :codcliente
       t.Integer :codvendedor
       t.string :subtotal
